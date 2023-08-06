9009f93d46e0aec3c89358881b3f692dfb128de6
OmarRayes98 <omarrayes1998@gmail.com>
2023-08-06 03:16:56 +0200

    edit some info of portofolio

--- src/data/user.js
+++ src/data/user.js
@@ -1,9 +1,9 @@
 const INFO = {
 	main: {
-		title: "Karam Mustafa",
-		name: "Tharindu N.",
-		email: "karam2mustafa@gmail.com",
-		logo: "../logo.png",
+		title: "Omar Rayes",
+		name: "Omar R.",
+		email: "omarrayes1998@gmail.com",
+		logo: "../fadv-portofolio-task/logo1.png",
 	},
 
 	socials: {
@@ -16,13 +16,13 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Full-stack web and mobile app developer, and amateur astronaut.",
-		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+		title: "Front-end web developer, and amateur astronaut. Seeking to be FullStack",
+		description: 
+			"I am a front-end developer with expertise in responsive screens . I have experience in dealing with APIS . I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
 
 	about: {
-		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		title: "I’m Omar R. I live in Syria Latakia, where I design the future.",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
